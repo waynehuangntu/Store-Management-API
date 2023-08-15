8fb736c0c307a2b16451f13a9a65a5b896b80e5e
Wayne Huang <wayne@WaynedeMacBook-Air.local>
2023-08-15 19:44:11 +0800

    First Commit

---
+++ resources/item.py
@@ -0,0 +1,63 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import jwt_required
+from db import db
+
+from schemas import ItemSchema, ItemUpdateSchema
+from models import ItemModel
+
+blp = Blueprint("Items", "items", description="Operations on items")
+
+
+@blp.route("/item/<int:item_id>")
+class Item(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema)
+    def get(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+    @jwt_required()
+    def delete(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."}
+
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self,item_data, item_id):
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
+
+        db.session.add(item)
+        db.session.commit()
+        return item
+    
+
+@blp.route("/item")
+class ItemList(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        return ItemModel.query.all()
+
+    @jwt_required(fresh=True) #needs fresh access token
+    @blp.arguments(ItemSchema) #ItemSchema validates the data for us and then forward the validated data into the following def
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
+        item = ItemModel(**item_data)
+
+        try:
+            db.session.add(item)
+            db.session.commit()#saving to database
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
+
+        return item
